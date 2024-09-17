a7d241a138115355794a032a6cf7c52245c97294
Mateus Chines <mateuschines>
2024-09-16 22:25:24 -0300

    Aula 16-09

--- src/pages/Usuarios/Gerenciar/index.tsx
+++ src/pages/Usuarios/Gerenciar/index.tsx
@@ -1,15 +1,16 @@
-import { useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom"
 import { IToken } from "../../../interfaces/token";
 import { verificaTokenExpirado } from "../../../services/token";
 import { LayoutDashboard } from "../../../components/LayoutDashboard";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
 
 interface IForm {
     nome: string
     email: string
-    senha: string
-    permissao: string
+    password: string
+    permissoes: string
 }
 
 export default function GerenciarUsuarios() {
@@ -19,6 +20,8 @@ export default function GerenciarUsuarios() {
         handleSubmit,
         formState: { errors }
     } = useForm<IForm>()
+
+    const refForm = useRef<any>();
 
     const navigate = useNavigate();
 
@@ -43,6 +46,20 @@ export default function GerenciarUsuarios() {
 
     }, [])
 
+    const submitForm: SubmitHandler<IForm> = useCallback(
+        (data) => {
+
+            axios.post('http://localhost:3001/users',
+                data
+            ).then((res) => {
+                navigate('/usuarios')
+            })
+                .catch((err) => {
+                    console.log(err)
+                })
+
+        }, [])
+
     return (
         <>
             <LayoutDashboard>
@@ -54,6 +71,16 @@ export default function GerenciarUsuarios() {
                     style={{
                         alignItems: 'center'
                     }}
+                    onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+                        event.preventDefault()
+
+                        refForm.current.classList.add('was-validated')
+
+                        handleSubmit(submitForm)(event)
+
+                    }}
+                    ref={refForm}
+                // ref={(valorReferenciaHtml) => { refForm.current = valorReferenciaHtml }}
                 >
                     <div className="col-md-12">
                         <label
@@ -69,11 +96,97 @@ export default function GerenciarUsuarios() {
                             id="nome"
                             required
                             {...register('nome',
-                                { required: 'Nome é obrigatório!' }
+                                {
+                                    required: 'Nome é obrigatório!',
+                                }
                             )}
                         />
                         <div className="invalid-feedback">
                             {errors.nome && errors.nome.message}
+                        </div>
+
+                    </div>
+
+                    <div className="col-md-12">
+                        <label
+                            htmlFor="email"
+                            className="form-label"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Yuri"
+                            id="email"
+                            required
+                            {...register('email',
+                                {
+                                    required: 'Email é obrigatório!',
+                                }
+                            )}
+                        />
+                        <div className="invalid-feedback">
+                            {errors.email && errors.email.message}
+                        </div>
+
+                    </div>
+
+                    <div className="col-md-12">
+                        <label
+                            htmlFor="permissoes"
+                            className="form-label"
+                        >
+                            Perfil
+                        </label>
+
+                        <select
+                            className="form-select"
+                            defaultValue={''}
+                            id="permissoes"
+                            required
+                            {
+                                ...register("permissoes",
+                                    {required: 'Selecione'}
+                                )
+                            }
+                        >
+                            <option value="">
+                                Selecione o tipo
+                            </option>
+                            <option value="admin">
+                                Admin
+                            </option>
+                            <option value="colaborador">
+                                Colaborador
+                            </option>
+                        </select>
+                        <div className="invalid-feedback">
+                            {errors.permissoes && errors.permissoes.message}
+                        </div>
+                    </div>
+
+                    <div className="col-md-12">
+                        <label
+                            htmlFor="password"
+                            className="form-label"
+                        >
+                            Senha
+                        </label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Yuri"
+                            id="password"
+                            required
+                            {...register('password',
+                                {
+                                    required: 'Senha é obrigatório!',
+                                }
+                            )}
+                        />
+                        <div className="invalid-feedback">
+                            {errors.password && errors.password.message}
                         </div>
 
                     </div>
